23d97f417810c54920db9057bd902b40aebbdad0
Besart Elezi <besart.elezi@hotmail.com>
2022-08-04 11:58:21 +0200

    updated friend class, added nullable properties

--- friendship-book/src/app/friend.ts
+++ friendship-book/src/app/friend.ts
@@ -1,2 +1,15 @@
 export class Friend {
+  firstName : string | null;
+  lastName : string | null;
+  email : string | null;
+  phoneNumber : number | null;
+  favouriteLanguage : string | null;
+
+  constructor(firstName : string | null, lastName : string | null, email : string | null, phoneNumber : number | null, favouriteLanguage : string | null) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.favouriteLanguage = favouriteLanguage;
+  }
 }
