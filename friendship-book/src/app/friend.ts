8b83564554684418bf87b0443b0aa31e8fb9d98a
Besart Elezi <besart.elezi@hotmail.com>
2022-08-04 15:49:18 +0200

    added regex code + required

--- friendship-book/src/app/friend.ts
+++ friendship-book/src/app/friend.ts
@@ -2,10 +2,10 @@ export class Friend {
   firstName : string | null;
   lastName : string | null;
   email : string | null;
-  phoneNumber : number | null;
+  phoneNumber : string | null;
   favouriteLanguage : string | null;
 
-  constructor(firstName : string | null, lastName : string | null, email : string | null, phoneNumber : number | null, favouriteLanguage : string | null) {
+  constructor(firstName : string | null, lastName : string | null, email : string | null, phoneNumber : string | null, favouriteLanguage : string | null) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
