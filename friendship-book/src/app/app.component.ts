07a1fb28621c09ae35319d0b7899f88bdde2d53a
Besart Elezi <besart.elezi@hotmail.com>
2022-08-08 15:43:10 +0200

    added get request to server from client

--- friendship-book/src/app/app.component.ts
+++ friendship-book/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Friend} from "./friend";
 import {AddFriendService} from "./add-friend.service";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,9 @@ import {AddFriendService} from "./add-friend.service";
 
 })
 
-
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  url : string = "http://localhost:9500/allfriends";
+  allFriends : Friend[] = [];
   languageArray = ['PHP', 'CSS', 'HTML', 'Javascript', 'C++', 'C#', 'Español', 'Java', 'Si++, the Spanish C++'];
   friend = new Friend(null, null, null, null, null);
 
@@ -25,6 +26,22 @@ export class AppComponent {
     console.log(this.friend)
     let observable =  this._addFriendService.addFriend(this.friend);
     console.log(observable);
-    observable.subscribe(data =>' it worked', error => "it didn't work");
+    observable.subscribe(data =>this.getFriendsOnLoad('http://localhost:9500/allfriends'), error => "it didn't work");
   }
+
+  public async getFriendsOnLoad (url : string) : Promise<any> {
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    this.allFriends = await response.json();
+    console.log(this.allFriends);
+  }
+
+  ngOnInit(): any {
+    this.getFriendsOnLoad('http://localhost:9500/allfriends');
+  }
+
 }
